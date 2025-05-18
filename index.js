d25172f08a119ef9363272ad28021aa7a954ba92
Olha Shapoval <belyaevaolya0@gmail.com>
2025-05-18 11:21:31 +0300

    Added health check

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ import campersRoutes from "./routes/campers.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+// В продакшн на Elastic Beanstalk краще слухати 80 порт
+const PORT = process.env.PORT || 80;
 
 app.use(cors());
 app.use(express.json());
+
+// Роут для перевірки здоров'я сервера (health check)
+app.get("/api/health", (req, res) => {
+  res.status(200).send("OK");
+});
 
 app.use("/api/campers", campersRoutes);
 
